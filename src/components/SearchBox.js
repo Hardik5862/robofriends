f16ea7837786e53354798dddef663739e6608945
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2021-01-24 20:37:48 +0530

    formatting files

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
-function SearchBox({ searchfield , searchChange}) {
-	return (
-		<div className='pa2'>
-			<input
-			  className='pa3 ba b--green bg-lightest-blue'
-			  type='search'
-			  placeholder='search for robots' 
-			  onChange={searchChange}
-			  />
-		</div>
-	);
+function SearchBox({ searchChange }) {
+  return (
+    <div className="pa2">
+      <input
+        aria-label="search robots"
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search for robots"
+        onChange={searchChange}
+      />
+    </div>
+  );
 }
 
 export default SearchBox;
